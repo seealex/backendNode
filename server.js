bfd6434bd55e286fdb1dd3b6b78d9089e9d41af8
Alex Silva <asilva3@uolinc.com>
2019-09-18 00:24:08 -0300

    v2

--- server.js
+++ server.js
@@ -23,6 +23,6 @@ app.get('/', (req, res) => {
 });
 
 // listen for requests
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server is listening on port 3000");
 });
